a4776db90bcd436153f1ebf5ed70b577931dfa2d
Josh Sideris <josh.sideris@gmail.com>
2017-01-19 00:58:53 -0500

    Fix a bunch of bugs regarding special tags + waits.

--- dothtml.js
+++ dothtml.js
@@ -1,8 +1,9 @@
-//Version 1.0.0
+//Version 1.1.0
 
 function _DOT(document){
 	this._document = document;
 	this._if = null;
+	this._pendingCalls = []; //Allows you to set parent attributes from children. Also allows for jquery helper calls.
 }
 
 _DOT.prototype._warnings = true;
@@ -14,18 +15,68 @@ _DOT.prototype._getNewDocument = function(){
 	return document.createElement("DOCUMENT");
 };
 
+_DOT.prototype._getAnInstance = function(){
+	if(this._document || this._pendingCalls.length > 0) return this;
+	else {
+		var d = new _DOT(null);
+		d._if = this._if;
+		return d;
+	};
+};
+
 _DOT.prototype._getLastChildOrNull = function(){
 	if(this._document && this._document.lastChild) return this._document.lastChild;
 	return null;
 };
 
-_DOT.prototype._evalContent = function(content){
-	if(typeof content === "string" || typeof content === "number" || typeof content === "boolean") return document.createTextNode(content); //Raw data
+_DOT.prototype.getLast = function(){
+	return this._getLastChildOrNull();
+}
+
+//before is passed in so that attributes or jquery wrappers can be associated with before's sibling, instead of inheritingParent, the default.
+_DOT.prototype._evalContent = function(content, /*inheritingParent, before,*/ pendingCalls){
+	if(typeof content === "string" || typeof content === "number" || typeof content === "boolean") { //Raw data
+		var nDot = new _DOT(this._getNewDocument());
+		nDot._document.innerHTML = content;
+		return nDot._document.childNodes;
+	}
+	else if(Object.prototype.toString.call( content ) === '[object Array]') { //Array
+		var nDot = new _DOT(this._getNewDocument());
+		for(var i = 0; i < content.length; i++){
+			nDot._appendOrCreateDocument(content[i]);
+		}
+		if(nDot._document) return nDot._document.childNodes;
+	}
 	else if(content && content.constructor && content.call && content.apply) //Function to evaluate
 	{
-		return this._evalContent(content());
+		return this._evalContent(content(), /*inheritingParent, before,*/ pendingCalls);
 	}
-	else if(content && content._document) return content._document.childNodes; //DOT
+	else if(content && content instanceof _DOT) { //DOT
+		for(var i = 0; i < content._pendingCalls.length; i++){
+			pendingCalls.push(content._pendingCalls[i]);
+		}
+		/*
+		var inheritTarget = (before ? before.previousSibling : null) || inheritingParent || this._document.lastChild;
+		if(inheritTarget && content._pendingCalls){ //Inherit jquery or attr actions from first child.
+			for(var i = 0; i < content._pendingCalls.length; i++){
+				var call = content._pendingCalls[i];
+				if(call.type == "attr") inheritTarget.setAttribute(call.name, call.params[0]);
+				else if(call.type == "jQuery wrapper"){
+					var jo = jQuery(inheritTarget);
+					jo[call.name].apply(jo, call.params);
+				}
+				else if(call.type == "jQuery event"){
+					var jo = jQuery(inheritTarget);
+					jo[call.name](handler);
+				}
+				else if(call.type == "wait"){
+					call.callback();
+				}
+			}
+		}
+		*/
+		if(content._document) return content._document.childNodes; //Return all the nodes in here.
+	} 
 	
 	return null;
 };
@@ -33,7 +84,36 @@ _DOT.prototype._evalContent = function(content){
 _DOT.prototype._appendOrCreateDocument = function(content, parentEl, beforeNode){
 	//Note: the stuff with setting parentEl to beforeNode's parent is due to a very strange bug where this._document gets set to some phantom document when the wait function is used inside a div like so: DOT.div(DOT.wait(100, "hello!")); Try it. 
 	parentEl = (beforeNode ? beforeNode.parentNode : null) || parentEl || this._document || this._getNewDocument();
-	var eContent = this._evalContent(content);
+	
+	var nd = this._document === parentEl ? this : new _DOT(parentEl);
+	nd._if = this._if;
+	var pendingCalls = []; //This will populate with pending calls.
+	var eContent = nd._evalContent(content, /*parentEl, beforeNode,*/ pendingCalls);
+	for(var i = 0; i < pendingCalls.length; i++){
+		var call = pendingCalls[i];
+		//Three possibilities.
+		//1. Use the pending call against the last sibling element, if one exists.
+		//2. Otherwise, use it on the current parent, if it's ready.
+		//3. Otherwise, save it as a pending call right here. // Don't think this ever happens.
+		var pendingCallTarget = (beforeNode ? (beforeNode.previousSibling || parentEl /*Since lastChild will be a timeout*/) : null /*1*/) || parentEl.lastChild /*2*/ || parentEl; 
+		if(pendingCallTarget && pendingCallTarget.tagName != "DOCUMENT"){
+			if(call.type == "attr") pendingCallTarget.setAttribute(call.name, call.params[0]);
+			else if(call.type == "jQuery wrapper"){
+				var jo = jQuery(pendingCallTarget);
+				jo[call.name].apply(jo, call.params);
+			}
+			else if(call.type == "jQuery event"){
+				var jo = jQuery(pendingCallTarget);
+				jo[call.name](handler);
+			}
+			else if(call.type == "wait"){
+				call.callback();
+			}
+		}
+		else{
+			nd._pendingCalls.push(call); /*3*/
+		}
+	}
 	if(eContent !== null){
 		if( eContent instanceof NodeList ) {
 			//for(var i = 0; i < eContent.length; i++){
@@ -47,10 +127,8 @@ _DOT.prototype._appendOrCreateDocument = function(content, parentEl, beforeNode)
 			else parentEl.appendChild(eContent);
 		}
 	}
-	
-	var nd = this._document === parentEl ? this : new _DOT(parentEl);
-	nd._if = this._if;
 	return nd;
+	//return this;
 };
 
 _DOT.prototype.el = function(tag, content){
@@ -64,7 +142,10 @@ _DOT.prototype.el = function(tag, content){
 
 _DOT.prototype.h = function(content){
 	var hDoc = this._getNewDocument();
-	hDoc.innerHTML = content;
+	var hDot = new _DOT(hDoc);
+	//if(typeof content === "string" || typeof content === "number" || typeof content === "boolean") hDoc.innerHTML = content; //Raw data
+	hDot._appendOrCreateDocument(content)
+	
 	var nDoc = this._document || this._getNewDocument();
 	while(hDoc.childNodes.length > 0){
 		nDoc.appendChild(hDoc.childNodes[0]);
@@ -83,6 +164,12 @@ _DOT.prototype.attr = function(attr, value){
 	if(this._document) {
 		var cn = this._document.childNodes;
 		if(cn.length > 0 && cn[cn.length - 1].setAttribute) cn[cn.length - 1].setAttribute(attr, value || attr); //||attr is for self-explaining attributes
+	}
+	else{
+		var pD = this._getAnInstance();
+		//if(!pD._pendingCalls.length > 0) pD._pendingCalls = [];
+		pD._pendingCalls.push({type: "attr", name: attr, params: [value]});
+		return pD;
 	}
 	return this;
 };
@@ -132,7 +219,7 @@ _DOT.prototype.each = function(objects, callback){
 _DOT.prototype.if = function(condition, callback){
 	if(condition) {
 		this._if = true;
-		return this._appendOrCreateDocument(callback);
+		return this._getAnInstance()._appendOrCreateDocument(callback);
 	}
 	else{
 		this._if = false;
@@ -150,7 +237,7 @@ _DOT.prototype.elseif = function(condition, callback){
 _DOT.prototype.else = function(callback){
 	if(!this._if){
 		this._if = null;
-		return this._appendOrCreateDocument(callback);
+		return this._getAnInstance()._appendOrCreateDocument(callback);
 	}
 	return this;
 };
@@ -162,23 +249,94 @@ _DOT.prototype.script = function(callback){
 _DOT.prototype.wait = function(timeout, callback){
 	var timeoutDot = this.el("timeout");
 	var timeoutNode = timeoutDot._document.lastChild;
-	setTimeout(function(){
-		timeoutDot._appendOrCreateDocument(callback, null, timeoutNode);
-		timeoutNode.remove();
-	}, timeout);
+	var startTimer = function(){
+		setTimeout(function(){
+			timeoutDot._appendOrCreateDocument(callback, null, timeoutNode);
+			timeoutNode.remove();
+		}, timeout);
+	}
+	
+	startTimer();
 	return timeoutDot;
 };
 
-_DOT.prototype._createElement = function(tag){
-	_DOT.prototype[tag] = function(c){return this.el(tag, c);}
+_DOT.prototype.createWidget = function(name, callback){
+	_DOT.prototype[name] = function(){
+		var ret = callback.apply(DOT, arguments);
+		//return this.h(callback.apply(DOT, arguments)); //Passes all tests, but we don't necissarilly want to force callback to be dot.
+		if(ret instanceof _DOT) return this._appendOrCreateDocument(ret);
+		else return ret;
+	};
 };
 
-_DOT.prototype._createAttribute = function(attribute){
-	_DOT.prototype[attribute] = function(value){return this.attr(attribute, value);}
+_DOT.prototype.createElement = function(tag){
+	_DOT.prototype[tag] = _DOT.prototype[tag + "E"] = function(c){return this.el(tag, c);}
 };
 
-//Hyphenated Attributes.
-_DOT.prototype["acceptcharset"] = _DOT.prototype["accept-charset"] = function(value){return this.attr("accept-charset", value);};
+_DOT.prototype.createAttribute = function(attribute){
+	_DOT.prototype[attribute] = _DOT.prototype[attribute + "A"] = function(value){return this.attr(attribute, value);}
+};
+
+_DOT.prototype.createJQueryWrapper = function(name){
+	_DOT.prototype["$" + name] = function(){
+		this.check$();
+		var jo = jQuery(this._getLastChildOrNull()); // Get this first because a timeout tag is going to be added.
+		
+		var timeoutDot = null;
+		var timeoutNode = null;
+		
+		//Replace function args with a custom wrapper.
+		for(var i = 0; i < arguments.length; i++){
+			var arg = arguments[i];
+			if(arg instanceof _DOT){
+				var dot = arg;
+				arg = function(){return dot;}
+			}
+			if(arg && arg.constructor && arg.call && arg.apply){
+				timeoutDot = this.el("timeout");
+				timeoutNode = timeoutDot._document.lastChild;
+				(function(arg, args, timeoutNode, timeoutDot){
+					args[i] = function(){
+						var ret = timeoutDot._appendOrCreateDocument(arg, null, timeoutNode);
+						timeoutNode.remove();
+					};
+				})(arg, arguments, timeoutNode, timeoutDot);
+				 break; //First function is assumed to be the callback.
+			}
+		}
+		
+		var retDOT = timeoutDot || this;
+		if(this._document){
+			
+			jo[name].apply(jo, arguments);
+			return retDOT;
+		}
+		else{
+			
+			var pD = (retDOT._pendingCalls.length > 0 ? retDOT : new _DOT(retDOT._document));
+			if(!pD._pendingCalls) pD._pendingCalls = [];
+			pD._pendingCalls.push({type: "jQuery wrapper", name: name, params: arguments});
+			return pD;
+		}
+	}
+};
+
+_DOT.prototype.createJQueryEventHandler = function(name){
+	_DOT.prototype["$" + name] = function(handler){
+		this.check$();
+		if(this._document){
+			var jo = jQuery(this._getLastChildOrNull());
+			jo[name](handler);
+			return this;
+		}
+		else{
+			var pD = (this._pendingCalls.length > 0 ? this : new _DOT(this._document));
+			if(!pD._pendingCalls) pD._pendingCalls = [];
+			pD._pendingCalls.push({type: "jQuery event", name: name, params: arguments});
+			return pD;
+		}
+	}
+};
 
 (function(){
 	var allTags = [
@@ -197,9 +355,11 @@ _DOT.prototype["acceptcharset"] = _DOT.prototype["accept-charset"] = function(va
 		"button",
 		"canvas",
 		"caption",
+		"cite", //*
 		"code",
 		"col",
 		"colgroup",
+		"data", //*
 		"datalist",
 		"dd",
 		"del",
@@ -215,6 +375,7 @@ _DOT.prototype["acceptcharset"] = _DOT.prototype["accept-charset"] = function(va
 		"figcaption",
 		"figure",
 		"footer",
+		"form", //*
 		"h1",
 		"h2",
 		"h3",
@@ -230,6 +391,7 @@ _DOT.prototype["acceptcharset"] = _DOT.prototype["accept-charset"] = function(va
 		"ins",
 		"kbd",
 		"keygen",
+		"label", //*
 		"legend",
 		"li",
 		"main",
@@ -259,8 +421,10 @@ _DOT.prototype["acceptcharset"] = _DOT.prototype["accept-charset"] = function(va
 		"select",
 		"small",
 		"source",
+		"span", //*
 		"strong",
 		"sub",
+		"summary", //*
 		"sup",
 		"table",
 		"tbody",
@@ -276,13 +440,7 @@ _DOT.prototype["acceptcharset"] = _DOT.prototype["accept-charset"] = function(va
 		"ul",
 		"var",
 		"video",
-		"wbr",
-		"dataE", //Special explicit 
-		"citeE",
-		"formE",
-		"labelE",
-		"spanE",
-		"summaryE"
+		"wbr"
 	];
 	
 	var allAttributes = [
@@ -308,6 +466,7 @@ _DOT.prototype["acceptcharset"] = _DOT.prototype["accept-charset"] = function(va
 		"char",
 		"charoff",
 		"checked",
+		"cite", //*
 		"class",
 		"classid",
 		"clear",
@@ -324,6 +483,7 @@ _DOT.prototype["acceptcharset"] = _DOT.prototype["accept-charset"] = function(va
 		"datetime",
 		"declare",
 		"default",
+		"data", //*
 		"dir",
 		"dirname",
 		"disabled",
@@ -341,6 +501,7 @@ _DOT.prototype["acceptcharset"] = _DOT.prototype["accept-charset"] = function(va
 		"fontspecification",
 		"for",
 		"foreignobject",
+		"form", //*
 		"formaction",
 		"frame",
 		"frameborder",
@@ -358,6 +519,7 @@ _DOT.prototype["acceptcharset"] = _DOT.prototype["accept-charset"] = function(va
 		"itemprop",
 		"keytype",
 		"kind",
+		"label", //*
 		"lang",
 		"list",
 		"longdesc",
@@ -423,6 +585,7 @@ _DOT.prototype["acceptcharset"] = _DOT.prototype["accept-charset"] = function(va
 		"shape",
 		"size",
 		"sizes",
+		"span", //*
 		"spellcheck",
 		"src",
 		"srcdoc",
@@ -431,6 +594,7 @@ _DOT.prototype["acceptcharset"] = _DOT.prototype["accept-charset"] = function(va
 		"standby",
 		"start",
 		"step",
+		"summary", //*
 		"style",
 		"tabindex",
 		"target",
@@ -441,27 +605,63 @@ _DOT.prototype["acceptcharset"] = _DOT.prototype["accept-charset"] = function(va
 		"value",
 		"valuetype",
 		"width",
-		"wrap",
-		"dataA", //Special explicit 
-		"citeA",
-		"formA",
-		"labelA",
-		"spanA",
-		"summaryA"
+		"wrap"
+		//"dataA", //Special explicit 
+		//"citeA",
+		//"formA",
+		//"labelA",
+		//"spanA",
+		//"summaryA"
 	];
 	
-	for(var i = 0; i < allTags.length; i++){
-		_DOT.prototype._createElement(allTags[i]);
-	}
+	var allJQueryWrappers = [
+		"animate",
+		"css",
+		"empty",
+		"fadeIn",
+		"fadeOut",
+		"fadeTo",
+		"hide",
+		"show"
+	];
 	
-	for(var i = 0; i < allAttributes.length; i++){
-		_DOT.prototype._createAttribute(allAttributes[i]);
-	}
+	var allJQueryEventHandlers = [
+		"blur",
+		"change",
+		"click",
+		"dblclick",
+		"focus",
+		"focusin",
+		"focusout",
+		"hover",
+		"keydown",
+		"keypress",
+		"keyup",
+		"mousedown",
+		"mouseenter",
+		"mouseleave",
+		"mousemove",
+		"mouseout",
+		"mouseover",
+		"mouseup",
+		"one",
+		"resize",
+		"scroll",
+		"select",
+		"submit"
+	];
+	
+	for(var i = 0; i < allTags.length; i++) _DOT.prototype.createElement(allTags[i]);
+	for(var i = 0; i < allAttributes.length; i++) _DOT.prototype.createAttribute(allAttributes[i]);
+	for(var i = 0; i < allJQueryWrappers.length; i++) _DOT.prototype.createJQueryWrapper(allJQueryWrappers[i]);
+	for(var i = 0; i < allJQueryEventHandlers.length; i++) _DOT.prototype.createJQueryEventHandler(allJQueryEventHandlers[i]);
 }());
+
+//Hyphenated Attributes.
+_DOT.prototype["acceptcharset"] = _DOT.prototype["accept-charset"] = function(value){return this.attr("accept-charset", value);};
+
 //SVG
 _DOT.prototype.svg = function(){throw "SVG is not supported by this library. Keep an eye out for dotsvg.js in the future."};
-
-//Attributes
 
 //Data is a special attribute.
 _DOT.prototype.data = function(suffix, value){
@@ -479,73 +679,62 @@ _DOT.prototype.data = function(suffix, value){
 //summary, span, label, form, cite
 
 _DOT.prototype.cite = function(arg){
-	var tagType = null;
-	if(jQuery(this._getLastChildOrNull()) != null)
-		tagType = jQuery(this._getLastChildOrNull())[0].nodeName.toLowerCase();
-	if(tagType != null && typeof arg !== "undefined" 
-		&& (tagType == "blockquote" 
-		|| tagType == "del" 
-		|| tagType == "ins" 
-		|| tagType == "q"))
-		return this.attr("cite", arg);
-	else
-		return this.el("cite", arg);
+	if(arg && this._document && this._document.lastChild){
+		var tagType = this._document.lastChild.tagName;
+		if(tagType == "BLOCKQUOTE" 
+			|| tagType == "DEL" 
+			|| tagType == "INS" 
+			|| tagType == "Q")
+			return this.attr("cite", arg);
+	}
+	return this.el("cite", arg);
 };
-
+		
 _DOT.prototype.form = function(arg){
-	var tagType = null;
-	if(jQuery(this._getLastChildOrNull()) != null)
-		tagType = jQuery(this._getLastChildOrNull())[0].nodeName.toLowerCase();
-	if(tagType != null && typeof arg !== "undefined" 
-		&& (tagType == "button" 
-		|| tagType == "fieldset" 
-		|| tagType == "input" 
-		|| tagType == "keygen" 
-		|| tagType == "label" 
-		|| tagType == "meter" 
-		|| tagType == "object" 
-		|| tagType == "output" 
-		|| tagType == "progress" 
-		|| tagType == "select" 
-		|| tagType == "textarea" 
-		))
+	if(arg && this._document && this._document.lastChild){
+		var tagType = this._document.lastChild.tagName;
+		if(tagType == "BUTTON" 
+			|| tagType == "FIELDSET" 
+			|| tagType == "INPUT" 
+			|| tagType == "KEYGEN"
+			|| tagType == "LABEL"
+			|| tagType == "METER"
+			|| tagType == "OBJECT"
+			|| tagType == "OUTPUT"
+			|| tagType == "PROGRESS"
+			|| tagType == "SELECT"
+			|| tagType == "TEXTAREA")
 			return this.attr("form", arg);
-	else
-		return this.el("form", arg);
+	}
+	return this.el("form", arg);
 };
 
 _DOT.prototype.label = function(arg){
-	var tagType = null;
-	if(jQuery(this._getLastChildOrNull()) != null)
-		tagType = jQuery(this._getLastChildOrNull())[0].nodeName.toLowerCase();
-	if(tagType != null && typeof arg !== "undefined" 
-		&& (tagType == "track"))
-		return this.attr("label", arg);
-	else
-		return this.el("label", arg);
+	if(arg && this._document && this._document.lastChild){
+		var tagType = this._document.lastChild.tagName;
+		if(tagType == "TRACK")
+			return this.attr("label", arg);
+	}
+	return this.el("label", arg);
 };
 
 _DOT.prototype.span = function(arg){
-	var tagType = null;
-	if(jQuery(this._getLastChildOrNull()) != null)
-		tagType = jQuery(this._getLastChildOrNull())[0].nodeName.toLowerCase();
-	if(tagType != null && typeof arg !== "undefined" 
-		&& (tagType == "select" 
-		|| tagType == "input"))
-		return this.attr("span", arg);
-	else
-		return this.el("span", arg);
-}
+	if(arg && this._document && this._document.lastChild){
+		var tagType = this._document.lastChild.tagName;
+		if(tagType == "COL" 
+			|| tagType == "COLGROUP")
+			return this.attr("span", arg);
+	}
+	return this.el("span", arg);
+};
 
-	this.summary = function(arg){
-	var tagType = null;
-	if(jQuery(this._getLastChildOrNull()) != null)
-		tagType = jQuery(this._getLastChildOrNull())[0].nodeName.toLowerCase();
-	if(tagType != null && typeof arg !== "undefined" 
-		&& (tagType == "table"))
-		return this.attr("summary", arg);
-	else
-		return this.el("summary", arg);	
+_DOT.prototype.summary = function(arg){
+	if(arg && this._document && this._document.lastChild){
+		var tagType = this._document.lastChild.tagName;
+		if(tagType == "TABLE")
+			return this.attr("summary", arg);
+	}
+	return this.el("summary", arg);
 };
 
 //Jquery wrappers
@@ -580,7 +769,7 @@ _DOT.prototype.$blur = function(handler){
 	return this;
 };
 
-_DOT.prototype.$change = function(handler){this.check$(); jQuery(this._getLastChildOrNull()).change(function(e){handler(e)}); return this;};
+/*_DOT.prototype.$change = function(handler){this.check$(); jQuery(this._getLastChildOrNull()).change(function(e){handler(e)}); return this;};
 _DOT.prototype.$click = function(handler){this.check$(); jQuery(this._getLastChildOrNull()).click(function(e){handler(e)}); return this;};
 _DOT.prototype.$dblclick = function(handler){this.check$(); jQuery(this._getLastChildOrNull()).dblclick(function(e){handler(e)}); return this;};
 _DOT.prototype.$focus = function(handler){this.check$(); jQuery(this._getLastChildOrNull()).focus(function(e){handler(e)}); return this;};
@@ -611,6 +800,6 @@ _DOT.prototype.$fadeIn = function(p1, p2){this.check$(); jQuery(this._getLastChi
 _DOT.prototype.$fadeOut = function(p1, p2){this.check$(); jQuery(this._getLastChildOrNull()).fadeIn(p1, p2); return this;};
 _DOT.prototype.$fadeTo = function(p1, p2, p3, p4){this.check$(); jQuery(this._getLastChildOrNull()).fadeTo(p1, p2, p3, p4); return this;};
 _DOT.prototype.$hide = function(p1, p2, p3){this.check$(); jQuery(this._getLastChildOrNull()).hide(p1, p2, p3); return this;};
-_DOT.prototype.$show = function(p1, p2, p3){this.check$(); jQuery(this._getLastChildOrNull()).show(p1, p2, p3); return this;};
+_DOT.prototype.$show = function(p1, p2, p3){this.check$(); jQuery(this._getLastChildOrNull()).show(p1, p2, p3); return this;};*/
 
 var DOT = new _DOT(null);
